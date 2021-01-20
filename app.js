b2f166f296634092ddc67328016e1155b4eca8a9
Ahmad Faaiz Al-auza'i <afaaiz2@gmail.com>
2021-01-20 17:41:15 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = 'mongodb+srv://root:root@cluster0.8et8p.mongodb.net/main?retryWrites=true&w=majority';
+const DATABASE_NAME = "main";
+
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("modul");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.post("/modul", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+app.get("/modul", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/modul/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
